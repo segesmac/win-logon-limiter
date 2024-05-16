e78f3c26ea81094959d69e8cbb21930f17fb2d67
Chris Segesman <segesmac@gmail.com>
2024-05-16 10:55:04 -0600

    Reorganizing tests directories

--- webobjects/tests/indexTest.php
+++ webobjects/tests/indexTest.php
@@ -7,31 +7,14 @@ class IndexTest extends PHPUnit\Framework\TestCase
 {
  public function testOutput()
  {
-    // Capture the output of hello.php
+    // Capture the output of index.php
     ob_start();
     include '/app/html/index.php';
     $output = ob_get_clean();
 
-    // Assert that the output is "Hello, Docker!"
-    $content = <<<'EOD'
-<html>
-<head>
-<script src="includes/jquery.min.js" />
-<link rel="stylesheet" href="includes/style.css" />
-
-</head>
-<body>
-<!--Your candidate is: <h1 id=list>-</h1> Make the locals proud.
-<h2 id=list2>-</h2> So-so-->
-<div id="timetable">No data received.</div>
-<div id="internetstatus">No data received.</div>
-<script src="includes/timetable.js" />
-</body>
-
-</html>
-
-EOD;
-    $this->assertEquals($content, $output);
+    // Assert that the output starts and ends with <html></html>
+    $this->assertStringStartsWith("<html>", trim($output));
+    $this->assertStringEndsWith("</html>", trim($output));
  }
 }
 ?>
