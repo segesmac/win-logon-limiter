3a93b37bb5f711bfc01db34fff6837a04a56b0bb
Chris Segesman <segesmac@gmail.com>
2024-06-25 03:47:43 -0600

    Attempting to run a nuget push

--- build_tasks_choco/020-push_choco_package.ps1
+++ build_tasks_choco/020-push_choco_package.ps1
@@ -8,15 +8,13 @@ $nupkgFiles = Get-ChildItem -Path $folderPath -Filter *.nupkg
 
 foreach ($nupkgFile in $nupkgFiles) {
     $fileFullPath = $nupkgFile.FullName
-    $packageName = $nupkgFile.BaseName
+    #$packageName = $nupkgFile.BaseName
 
-    $fullApiUrl = "$apiUrl/api/packages/$feedName/upload/$packageName.nupkg"
-
-    $result = Invoke-WebRequest -Uri $fullApiUrl -Headers @{"X-ApiKey" = $apiKey} -Method POST -InFile $fileFullPath
-
-    if ($result.StatusCode -eq 200) {
-        Write-Host "Package '$packageName' uploaded successfully."
-    } else {
-        Write-Host "Failed to upload package '$packageName'. Status code: $($result.StatusCode)"
-    }
+    nuget push $fileFullPath -k $apiKey -s "$apiUrl/nuget/$feedName/"
+    #if ($result.StatusCode -eq 200) {
+    #    Write-Host "Package '$packageName' uploaded successfully."
+    #} else {
+    #    Write-Host "Failed to upload package '$packageName'. Status code: $($result.StatusCode)"
+    #}
 }
+
