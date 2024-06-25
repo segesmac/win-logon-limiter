7f04d37cf7244cc980376fb3ef0754fcd3d23848
Chris Segesman <segesmac@gmail.com>
2024-06-25 03:01:12 -0600

    Fixed missing binaries folder

--- build_tasks_choco/010-build_choco_package.ps1
+++ build_tasks_choco/010-build_choco_package.ps1
@@ -5,10 +5,12 @@ Set-Location $choco_dir
 (Get-Content tools/chocolateyInstall.ps1).Replace('__VERSION__', $env:VERSION_NUMBER) | Set-Content tools/chocolateyInstall.ps1
 $choco_zip_folder = Join-Path '/' 'choco_zip'
 New-Item -ItemType Directory $choco_zip_folder -Force
+$binaries_folder = Join-Path $choco_dir "binaries"
+New-Item -ItemType Directory $binaries_folder -Force
 Copy-Item ../clientobjects/scripts/heartbeat.ps1 $choco_zip_folder/heartbeat.ps1
 Copy-Item -Recurse ../clientobjects/scripts/config $choco_zip_folder/
-Compress-Archive -Path $choco_zip_folder -DestinationPath "binaries/$($env:VERSION_NUMBER)_winlogonlimiter.zip"
-$file_hash = Get-FileHash -Algorithm SHA256 "binaries/$($env:VERSION_NUMBER)_winlogonlimiter.zip"
-"$($file_hash.Hash)  $($env:VERSION_NUMBER)_winlogonlimiter.zip" | Out-File "binaries/$($env:VERSION_NUMBER)_SHA256SUMS"
+Compress-Archive -Path $choco_zip_folder -DestinationPath "$binaries_folder/$($env:VERSION_NUMBER)_winlogonlimiter.zip"
+$file_hash = Get-FileHash -Algorithm SHA256 "$binaries_folder/$($env:VERSION_NUMBER)_winlogonlimiter.zip"
+"$($file_hash.Hash)  $($env:VERSION_NUMBER)_winlogonlimiter.zip" | Out-File "$binaries_folder/$($env:VERSION_NUMBER)_SHA256SUMS"
 
 nuget pack
