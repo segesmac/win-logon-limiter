d43bd80109e48f75b4cf17610cb5f835e09a8e19
Chris Segesman <csegesman@ipro.com>
2024-07-03 17:32:23 -0600

    Adding temp admin infra to db

---
+++ webobjects/phpunit/inttests/heartbeatv1Test.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '/var/www/html/api/v1/heartbeat.php';
+
+#[PHPUnit\Framework\Attributes\CoversNothing]
+class ConnectTest extends PHPUnit\Framework\TestCase
+{
+ public function testOutput()
+ {
+    // Capture the output of connect.php
+    ob_start();
+    include '/var/www/html/api/v1/heartbeat.php';
+    $output = ob_get_clean();
+
+    // Assert that the output starts with Connection failed:, since there's no db to connect to
+    $this->assertStringStartsNotWith("Connection failed: ", $output);
+    $this->assertStringStartsNotWith("You must include a password variable", $output);
+ }
+}
+?>
